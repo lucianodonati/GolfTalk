fe4453dac620fdbd15b2d7806f6f4781366e67eb
Luciano Donati <me@lucianodonati.com>
2020-06-19 18:30:38 -0300

    agregado SFX manager

---
+++ Assets/Scripts/SfxManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SfxManager : Singleton<SfxManager>
+{
+    [SerializeField]
+    private AudioClip[] ballHitClips = null;
+
+    [SerializeField, HideInInspector]
+    private AudioSource aSource = null;
+
+    private void OnValidate()
+    {
+        if (null == aSource)
+            aSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayHitBall()
+    {
+        aSource.clip = ballHitClips[0];
+        aSource.PlayScheduled(AudioSettings.dspTime);
+    }
+}
