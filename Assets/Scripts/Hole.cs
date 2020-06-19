25dd120b52022e03e7028a499dba2216877cc1a6
Luciano Donati <me@lucianodonati.com>
2020-06-19 18:28:25 -0300

    Agregado script de los hoyos

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Hole : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particlesPrefab = null;
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        var ps = Instantiate(particlesPrefab, transform);
+        yield return new WaitForSeconds(2);
+        ps.Stop();
+    }
+}
